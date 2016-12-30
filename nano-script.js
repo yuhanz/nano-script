cd7aa2448319a6e980ef6b8c982800fab4e2f4c2
Zhang Yuhan <yuhanz@gmail.com>
2016-12-30 00:21:46 -0800

    refactor

--- nano-script.js
+++ nano-script.js
@@ -28,153 +28,153 @@
  * Date: 11/18/2016
  */
 
-function str2set(s) {
-   return s.split("").reduce(function(a,b) {a[b]=1; return a;}, {})
-}
-
-var syms = str2set("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_$.")
-var ops = str2set("+-*/=><&|:!?")
-var brs = str2set("[](){},")
-var des = str2set("\n;")
-var qus = str2set("\"'")
-
-var precedents = [
-  "&&", "||",
-  ">=", "<=", ">", "<", "==",
-  "+", "-",
-  "*", "/",
-  "()"]
-
-
-Nano = {
-  "tokenize": function(str) {
-    tokens = [];
-    var s;
-    exp = null;
-    var quo = null;
-    for(var i=0;i<str.length+1;i++) {
-      c = str[i]
-      t = syms[c] ? "s" : ops[c] ? "o" : null;
-      if(exp != null) {
-        if(exp == t || quo && quo != c) {
-          s += c;
-          continue
-        } else {
-          if(quo) {
-            s += quo
-            c = null
-          }
-          tokens.push(s)
-          exp = null;
-          quo = null;
-          s = ""
-          if(!c) {
-            continue
-          }
-        }
-      }
-
-      if(t == ' ') {
-        continue;
-      }
-      if(!t) {
-        if(brs[c]) {
-            tokens.push(c)
-        } else if(des[c]) {
-            tokens.push(";")
-        } else if(qus[c]) {
-            quo = exp = c;
-            s = c
-        }
-      } else {
-        s = c
-        exp = t
-      }
-    }
-    return tokens;
-  }
-}
-
-
-function chain(op, token, exp) {
-  if(Array.isArray(exp)) {
-    var nop = exp[0]
-    if(precedents.indexOf(op) > precedents.indexOf(nop)) {
-      return [nop, chain(op, token, exp[1]), exp[2]]
-    }
-  }
-  return [op, token, exp]
-}
-
-// exp: s
-// exp: s[exp]
-// exp: s(exp,exp,exp...)
-// exp: o exp
-// exp: (exp)
-// exp: exp o exp
-// exp: exp ? exp : exp
-// exp: exp .s
-function expression(tokens) {
-    var to = tokens[0]
-    var c = to[0]
-    if(syms[c]) {
-      tokens.shift()
-      var to2 = tokens[0]
-      if(!to2 || str2set("];,)")[to2[0]]) {
-        return to
-      }
-      var c2 = to2[0]
-      if(ops[c2]) {
-        var operator = tokens.shift()
-        var exp = expression(tokens)
-        return chain(operator, to, exp)
-      } else if(c2 == "[") {
-        tokens.shift()
-        var exp = expression(tokens)
-        if("]" != tokens.shift()) {
-          throw "brackets are not balanced"
-        }
-        exp = ["[]", to, exp]
-        if(!tokens.length) {
-          return exp
-        }
-        if(ops[tokens[0][0]]) {
-          operator = tokens.shift()
-          return [operator, exp, expression(tokens)]
-        }
-      } else if(c2 == "(") {
-        tokens.shift()
-        // function
-        var args = []
-        do {
-          args.push(expression(tokens))
-          var t = tokens.shift()
-        } while(t == ",");
-        if(t != ")") {
-          throw "parenthesis for function is not closed"
-        }
-        return ["func", to, args]
-      }
-    } else if(ops[c]) {
-      // operator
-      tokens.shift()
-      var exp = expression(tokens)
-      return [to, exp]
-    } else if(c == "(") {
-      tokens.shift()
-      var exp = expression(tokens)
-      if(")" != tokens.shift()) {
-        throw "parenthesis are not balanced"
-      }
-      return ["()", exp]
-    } else {
-      throw "illegal start of expression: " + to
-    }
-}
-
-
 function NanoContext() {
   this.variables = [];
+
+  function str2set(s) {
+     return s.split("").reduce(function(a,b) {a[b]=1; return a;}, {})
+  }
+
+  var syms = str2set("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_$.")
+  var ops = str2set("+-*/=><&|:!?")
+  var brs = str2set("[](){},")
+  var des = str2set("\n;")
+  var qus = str2set("\"'")
+
+  var precedents = [
+    "&&", "||",
+    ">=", "<=", ">", "<", "==",
+    "+", "-",
+    "*", "/",
+    "()"]
+
+    function tokenize(str) {
+      tokens = [];
+      var s;
+      exp = null;
+      var quo = null;
+      for(var i=0;i<str.length+1;i++) {
+        c = str[i]
+        t = syms[c] ? "s" : ops[c] ? "o" : null;
+        if(exp != null) {
+          if(exp == t || quo && quo != c) {
+            s += c;
+            continue
+          } else {
+            if(quo) {
+              s += quo
+              c = null
+            }
+            tokens.push(s)
+            exp = null;
+            quo = null;
+            s = ""
+            if(!c) {
+              continue
+            }
+          }
+        }
+
+        if(t == ' ') {
+          continue;
+        }
+        if(!t) {
+          if(brs[c]) {
+              tokens.push(c)
+          } else if(des[c]) {
+              tokens.push(";")
+          } else if(qus[c]) {
+              quo = exp = c;
+              s = c
+          }
+        } else {
+          s = c
+          exp = t
+        }
+      }
+      return tokens;
+    }
+
+  this.tokenize = tokenize;
+
+  function chain(op, token, exp) {
+    if(Array.isArray(exp)) {
+      var nop = exp[0]
+      if(precedents.indexOf(op) > precedents.indexOf(nop)) {
+        return [nop, chain(op, token, exp[1]), exp[2]]
+      }
+    }
+    return [op, token, exp]
+  }
+
+
+  // exp: s
+  // exp: s[exp]
+  // exp: s(exp,exp,exp...)
+  // exp: o exp
+  // exp: (exp)
+  // exp: exp o exp
+  // exp: exp ? exp : exp
+  // exp: exp .s
+  function expression(tokens) {
+      var to = tokens[0]
+      var c = to[0]
+      if(syms[c]) {
+        tokens.shift()
+        var to2 = tokens[0]
+        if(!to2 || str2set("];,)")[to2[0]]) {
+          return to
+        }
+        var c2 = to2[0]
+        if(ops[c2]) {
+          var operator = tokens.shift()
+          var exp = expression(tokens)
+          return chain(operator, to, exp)
+        } else if(c2 == "[") {
+          tokens.shift()
+          var exp = expression(tokens)
+          if("]" != tokens.shift()) {
+            throw "brackets are not balanced"
+          }
+          exp = ["[]", to, exp]
+          if(!tokens.length) {
+            return exp
+          }
+          if(ops[tokens[0][0]]) {
+            operator = tokens.shift()
+            return [operator, exp, expression(tokens)]
+          }
+        } else if(c2 == "(") {
+          tokens.shift()
+          // function
+          var args = []
+          do {
+            args.push(expression(tokens))
+            var t = tokens.shift()
+          } while(t == ",");
+          if(t != ")") {
+            throw "parenthesis for function is not closed"
+          }
+          return ["func", to, args]
+        }
+      } else if(ops[c]) {
+        // operator
+        tokens.shift()
+        var exp = expression(tokens)
+        return [to, exp]
+      } else if(c == "(") {
+        tokens.shift()
+        var exp = expression(tokens)
+        if(")" != tokens.shift()) {
+          throw "parenthesis are not balanced"
+        }
+        return ["()", exp]
+      } else {
+        throw "illegal start of expression: " + to
+      }
+  }
+
+  this.expression = expression;
 
   this.interpret = function(expression) {
     if(typeof expression == 'string') {
@@ -204,6 +204,7 @@ function NanoContext() {
 
     // array
     var op = expression[0]
+
     if(op == '=') {
       var n = expression[1];
       var v = this.interpret(expression[2]);
--- test/test.js
+++ test/test.js
@@ -8,21 +8,21 @@ vm.runInThisContext(code);
 var assert = require('assert');
 describe('Nano.tokenize', function() {
     it('should return tokens', function() {
-      ts = Nano.tokenize("abc.name=123.05")
+      ts = new NanoContext().tokenize("abc.name=123.05")
       assert.deepEqual(ts, ["abc.name", "=", "123.05"]);
-      ts = Nano.tokenize("abc.name = 123.05")
+      ts = new NanoContext().tokenize("abc.name = 123.05")
       assert.deepEqual(ts, ["abc.name", "=", "123.05"]);
 
-      ts = Nano.tokenize("abc.name = 123.05 + 'abc' + \"def\";")
+      ts = new NanoContext().tokenize("abc.name = 123.05 + 'abc' + \"def\";")
       assert.deepEqual(ts, ["abc.name", "=", "123.05", "+", "'abc'", "+", "\"def\"", ";"]);
 
-      ts = Nano.tokenize("numbers[x].value=10;");
+      ts = new NanoContext().tokenize("numbers[x].value=10;");
       assert.deepEqual(ts, ["numbers", "[", "x", "]", ".value", "=", "10", ";"]);
 
-      ts = Nano.tokenize("x = [1,m[0]]");
+      ts = new NanoContext().tokenize("x = [1,m[0]]");
       assert.deepEqual(ts, ["x", "=", "[", "1", ",", "m", "[","0", "]", "]" ]);
 
-      ts = Nano.tokenize("abc.name=123.05 + 'abc' + \"def\"; numbers[x].value=10; x = [1,m[0]]")
+      ts = new NanoContext().tokenize("abc.name=123.05 + 'abc' + \"def\"; numbers[x].value=10; x = [1,m[0]]")
       assert.deepEqual(ts, ["abc.name", "=", "123.05", "+", "'abc'", "+", "\"def\"", ";",
                         "numbers", "[", "x", "]", ".value", "=", "10", ";",
                         "x", "=", "[", "1", ",", "m", "[","0", "]", "]"]);
@@ -31,52 +31,52 @@ describe('Nano.tokenize', function() {
 
 describe('Nano.expression', function() {
     it('should parse expressions', function() {
-      exp = expression(["10"])
+      exp = new NanoContext().expression(["10"])
       assert.deepEqual(exp, "10")
-      exp = expression(["a", "=", "10"])
+      exp = new NanoContext().expression(["a", "=", "10"])
       assert.deepEqual(exp, ["=", "a", "10"])
-      exp = expression(["a", "[", "3", "]"])
+      exp = new NanoContext().expression(["a", "[", "3", "]"])
       assert.deepEqual(exp, ["[]", "a", "3"])
-      exp = expression(["a", "[", "3", "]", "=", "10"])
+      exp = new NanoContext().expression(["a", "[", "3", "]", "=", "10"])
       assert.deepEqual(exp, ["=", ["[]", "a", "3"], "10"])
 
-      exp = expression(["!", "a"])
+      exp = new NanoContext().expression(["!", "a"])
       assert.deepEqual(exp, ["!", "a"])
 
-      exp = expression(["!", "!", "a"])
+      exp = new NanoContext().expression(["!", "!", "a"])
       assert.deepEqual(exp, ["!", ["!", "a"]])
 
-      exp = expression(["a", "=", "b", "+", "10", "-", "x"])
+      exp = new NanoContext().expression(["a", "=", "b", "+", "10", "-", "x"])
       assert.deepEqual(exp, [ '=', 'a', [ '+', 'b', [ '-', '10', 'x' ]]])
     });
 
     it('should parse expressions with precedent', function() {
-      exp = expression(["a", "=", "b", "*", "10", "-", "x"])
+      exp = new NanoContext().expression(["a", "=", "b", "*", "10", "-", "x"])
       assert.deepEqual(exp, [ '=', 'a', [ '-', ['*', 'b', '10'], 'x' ]])
 
-      exp = expression(["a", "=", "c", "+", "b", "*", "10", "-", "x"])
+      exp = new NanoContext().expression(["a", "=", "c", "+", "b", "*", "10", "-", "x"])
       assert.deepEqual(exp, [ '=', 'a', ['+', 'c', [ '-', ['*', 'b', '10'], 'x' ]]])
 
-      exp = expression(["a", "=", "c", "+", "b", ">=", "10", "&&", "x"])
+      exp = new NanoContext().expression(["a", "=", "c", "+", "b", ">=", "10", "&&", "x"])
       assert.deepEqual(exp, [ '=', 'a', ['&&', ['>=', ['+', 'c', 'b'], '10'], 'x' ]])
 
     });
 
     it('should parse function', function() {
-      exp = expression(["f", "(", "a", ",", "b", ")"])
+      exp = new NanoContext().expression(["f", "(", "a", ",", "b", ")"])
       assert.deepEqual(exp, ["func", "f", ["a", "b"]])
     });
 
     it('should parse ternary expression', function() {
-      exp = expression(["c", "?", "0", ":", "1"])
+      exp = new NanoContext().expression(["c", "?", "0", ":", "1"])
       assert.deepEqual(exp, ["?", "c", [":", "0", "1"]])
     });
 
     it('should parse parenthese', function() {
-      exp = expression(["(", "10", ")"])
+      exp = new NanoContext().expression(["(", "10", ")"])
       assert.deepEqual(exp, ["()", "10"])
 
-      exp = expression(["a", "*", "(", "10", "+", "b", ")"])
+      exp = new NanoContext().expression(["a", "*", "(", "10", "+", "b", ")"])
       assert.deepEqual(exp, ["*", "a", ["()", ["+", "10", "b"]]])
     });
 
@@ -93,12 +93,12 @@ describe('Nano.expression', function() {
     // });
 
     it('should parse function', function() {
-      exp = expression(["f", "(", "a", ",", "b", ")"])
+      exp = new NanoContext().expression(["f", "(", "a", ",", "b", ")"])
       assert.deepEqual(exp, ["func", "f", ["a", "b"]])
     });
 
     it('should parse ternary expression', function() {
-      exp = expression(["c", "?", "0", ":", "1"])
+      exp =  new NanoContext().expression(["c", "?", "0", ":", "1"])
       assert.deepEqual(exp, ["?", "c", [":", "0", "1"]])
     });
 
